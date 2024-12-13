c5c33584d53098890b6068f27f2c1b880ad1f278
Davide Galvan <davide.galv@gmail.com>
2024-12-13 19:15:03 +0200

    test

--- .github/actions/unity-log-parser-action/unity-log-parser.py
+++ .github/actions/unity-log-parser-action/unity-log-parser.py
@@ -43,7 +43,14 @@ def set_github_output(key, value):
     with open("output.txt", "a") as f:
         if isinstance(value, list):
             for item in value:
-                f.write(f"{key}={item}\n")
+                """ 
+                Multiline strings output
+                {name}<<{delimiter}
+                {value}
+                {delimiter} 
+                """
+                f.write(f"{key}<<EOF\n{item}\nEOF\n")
+                # f.write(f"{key}={item}\n")
 
 if __name__ == "__main__":
     # Read log file path from input arguments
